18afd8a65d6679d6acdada6b93dfa4559a5fc9be
shannadige <hello@shannadige.com>
2025-05-21 23:19:37 -0500

    v0.1.2

--- src/app/layout.config.tsx
+++ src/app/layout.config.tsx
@@ -11,15 +11,14 @@ export const baseOptions: BaseLayoutProps = {
   nav: {
     title: (
       <>
-        <svg
-          width="24"
-          height="24"
-          xmlns="http://www.w3.org/2000/svg"
-          aria-label="Logo"
-        >
-          <circle cx={12} cy={12} r={12} fill="currentColor" />
-        </svg>
-        My App
+        <img
+          src="/othello.png"
+          alt="Othello Logo"
+          width={48}
+          height={48}
+          style={{ display: 'inline-block', verticalAlign: 'middle', marginRight: 0, marginTop: 16, marginBottom: 16 }}
+        />
+        Othello
       </>
     ),
   },
